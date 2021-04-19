53e2fdc3edc24e30ab9a043a9734dd9abb69d49e
CristinaDAmico <crydamico94@gmail.com>
2021-04-19 18:59:42 +0200

    print icons

---
+++ js/main.js
@@ -0,0 +1,140 @@
+/***********************
+    ICONS DISPLAY
+***********************/
+
+// Icons set (ipotetica sorgente esterna di dati)
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+    },
+];
+
+// Icon container 
+const container = document.querySelector('.icons');
+//console.log(container);
+
+// 1. Printare le icone a schermo
+printIcons(icons, container);
+
+
+/***********************
+    FUNCTIONS
+***********************/
+
+// Print icons on screen
+function printIcons(icons, container) {
+    // Generare il markup delle icone
+    let html = '';
+    icons.forEach( (icon) => {
+        // destrutturazione
+        const {family, prefix, name} = icon;
+
+        html += 
+        `<div class="icon p-20">
+            <i class="${family} ${prefix}${name}" style="color: #333"></i>
+            <div class="title">${name}</div>
+        </div>`;
+    });
+
+    // Aggiunta delle icone all interno del container icons
+    //console.log(html);
+    container.innerHTML = html;
+}
+
+
+
+
+
